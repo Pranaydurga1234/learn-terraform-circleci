9481a30953c5080879388e5cd2e47ac41d70832e
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-24 17:29:37 +0530

    working commit

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.44.0"
     }
   }
+  backend "s3" {
+        bucket = "circle-ci-backend-20220124114226560700000001"
+        key = "terraform/webapp/terraform.tfstate"
+        region = "us-east-1"
+       
+   }
   required_version = "> 0.14"
 }
 
