04efb0f6088dbe220b63dd95e0976eb446a62a5a
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-28 13:26:20 +0530

    fresh2

--- cluster.tf
+++ cluster.tf
@@ -80,6 +80,12 @@ resource "aws_eks_node_group" "node" {
   node_group_name = "node_demo"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = ["subnet-0b2dd1147aca5c0c6", "subnet-0416c21d6c0bbe90b"]
+  instance_types = var.instance_types
+  capacity_type = var.capacity_type
+
+  remote_access{
+      ec2_ssh_key = var.ec2_ssh_key
+  }
 
   scaling_config {
     desired_size = 1
