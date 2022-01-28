d00ba8b866a07c941e82463bbaba2e59a94d4598
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-28 18:32:49 +0530

    will1

--- versions.tf
+++ versions.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.20.0"
-    }
-
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
