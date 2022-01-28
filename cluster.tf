fe0588c674fcd4011b7a9d03b31205cb2d07e64d
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-28 12:26:56 +0530

    cluster changed16

--- cluster.tf
+++ cluster.tf
@@ -80,10 +80,8 @@ resource "aws_eks_node_group" "node" {
   node_group_name = "node_demo"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = ["subnet-0b2dd1147aca5c0c6", "subnet-0416c21d6c0bbe90b"]
-  
-compute = {
-     capacity_type = "Open"
-   }
+  capacity_type = "Open"
+
   scaling_config {
     desired_size = 1
     max_size     = 2
