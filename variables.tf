95dff5cd941202bc47437f40d988bab93e03b646
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-28 13:29:54 +0530

    fresh4

--- variables.tf
+++ variables.tf
@@ -2,4 +2,14 @@ variable "region" {}
 variable "user" {}
 variable "label" {}
 variable "app" {}
+variable "instance_types" {
+    type = list
+}
 
+variable "capacity_type" {
+    default = " "
+}
+
+variable "ec2_ssh_key" {
+    default = " "
+}
