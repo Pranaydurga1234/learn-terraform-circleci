2d0842ab5c015bb79c796c42c29f9077c04f9ebd
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-01-28 11:34:56 +0530

    cluster changed11

--- cluster.tf
+++ cluster.tf
@@ -75,20 +75,22 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 }
 
 resource "aws_eks_node_group" "node" {
+  instance_types  = t2.micro
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node_demo"
   node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = ["subnet-0b2dd1147aca5c0c6", "subnet-0416c21d6c0bbe90b"]
+  create_before_destroy = true
+  kubernetes_version    = var.kubernetes_version == null || var.kubernetes_version == "" ? [] : [var.kubernetes_version]
+  cluster_autoscaler_enabled = var.autoscaling_policies_enabled
+  context = module.label.context
 
   scaling_config {
     desired_size = 1
     max_size     = 1
     min_size     = 1
   }
- 
- lifecycle {
-    ignore_changes = [scaling_config[0].desired_size]
-  }
+
   
 
   depends_on = [
