353fbd85da1f6887eec6ad8371c6ba2cee9f607d
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-02-02 09:44:52 +0530

    cloud3

--- variables.tf
+++ variables.tf
@@ -2,16 +2,3 @@ variable "region" {}
 variable "user" {}
 variable "label" {}
 variable "app" {}
-variable "domain_name" {
-  type        = string
-  description = "The domain name for the website."
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "The name of the bucket without the www. prefix. Normally domain_name."
-}
-
-variable "common_tags" {
-  description = "Common tags you want applied to all components."
-}
