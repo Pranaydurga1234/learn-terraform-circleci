de323060008dbb1f644cb4ecfedf1abdc96155b5
pranaydurga1234 <pranayadurga,pondala@gmail.com>
2022-02-03 12:04:41 +0530

    project7

--- cloudfront.tf
+++ cloudfront.tf
@@ -16,8 +16,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     domain_name = aws_s3_bucket.b.bucket_regional_domain_name
     origin_id   = "${local.s3_origin_id}"
 
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
